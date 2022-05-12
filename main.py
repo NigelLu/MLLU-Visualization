d9d277451cc4dc12adb7fe07b376004b7eb0e956
NigelLu <nigellu@outlook.com>
2022-05-12 12:48:12 -0400

    Updated code to draw plots for em, f1, and avg

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pandas as pd
 from utils.drawline import drawline
 
 # read data
-df = pd.read_csv('Phase1.csv')
+df_EM = pd.read_csv('Phase1-EM.csv')
+df_F1 = pd.read_csv('Phase1-F1.csv')
+df_AVG = pd.read_csv('Phase1-Avg.csv')
 
-drawline(data=df, x_label="Percentage", save_config=[True, "./test3.png"])
+drawline(data=df_EM, x_label="Percentage", save_config=[True, "phase1-horizontal-em.png"])
+drawline(data=df_F1, x_label="Percentage", save_config=[True, "phase1-horizontal-f1.png"])
+drawline(data=df_AVG, x_label="Percentage", save_config=[True, "phase1-horizontal-avg.png"])
